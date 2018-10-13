bf89d0a7fbaff26c83f17c08ab5e7063dd96dab8
System Administrator <root@BAHATIs-MacBook-Pro.local>
2018-10-13 09:27:13 +0200

    push static images

--- index.php
+++ index.php
@@ -5,13 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="favicon.ico" rel="icon"> 
+    <link href="static/styles/main.css" rel="stylesheet" type="text/css"> 
     <title>Missionnaire network</title>
 </head>
 <body>
     
 
 
+    <section class="content-section">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <aside class="main-reading-section">
+            <div class="text-box">
+                <div class="text-box--title"></div>
+                <p class="text-box--content">Je vous salue tous au Nom de Notre Seigneur Jésus-Christ!
 
+                J' ai jugé utile le lancement du web site "missionnaire.net" pour ajouter cette voix à la voix d' autres frères qui crient pour que cette Épouse de Jésus-Christ puisse achever sa préparation pour le jour de la Venue du Seigneur.
+
+                La Bible dit clairement quelle sera la position de l'Église lorsque Jésus-Christ reviendra pour prendre Son Épouse. Elle ne laisse pas de place pour des suppositions et des interprétations. "Réjouissons-nous et tressaillons de joie, et donnons-Lui gloire; car les noces de l'Agneau sont venues; et Sa Femme s'est préparée; et il lui a été donné d'être vêtue de fin lin, éclatant et pur, car le fin lin, ces sont les justices des saints. Et Il me dit: Ecris: Bienheureux ceux qui sont conviés au banquet des noces de l'Agneau. Et Il me dit: Ce sont ici les véritables Paroles de Dieu." (Apoc. 19:7-9)
+
+                L'Épouse sera prête et ce sera une oeuvre de Dieu! "...et il lui a été donné d'être vêtue de fin lin,..." Il n'est donc pas question de créer des écoles ou des tribunaux mais ce sera une oeuvre extraordinaire du Saint-Esprit, agissant puissamment au milieu du peuple de Dieu qui a discerné et accepté l’action du rétablissement de l'Église de Jésus-Christ. Cela est en marche maintenant même. Nous assistons à l'impact que produit la Puissance de La Parole révélée! Ce "moureh", cette pluie d'enseignement comme le disait notre frère bien-aimé William Branham dans la prédication "La Parole parlée est la semence originelle" nous aligne sur la Parole de Dieu, nous plaçant ainsi là où nous devons être, savoir en Jésus-Christ! </p>
+            </div>
+        </aside>
+        <aside class="aside-useful-info">
+            info
+        </aside>
+    </section>
 
 
 
